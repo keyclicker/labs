6e2919f7821ab009700d756a4fde8cf6d757ed12
keyclicker <kolchikcv@ukr.net>
2020-06-09 20:55:27 +0300

    Lab4: demo

--- semester2/Lab4/FileSystem.hpp
+++ semester2/Lab4/FileSystem.hpp
@@ -3,32 +3,139 @@
 #include <vector>
 #include <memory>
 
+struct Time {
+  uint8_t day, month;
+  uint16_t year;
 
+  Time(uint16_t day = 0, uint16_t month = 0, uint16_t year = 0) :
+  day(day), month(month), year(year) {}
+
+  bool operator==(const Time &rhs) const {
+    return day == rhs.day && month == rhs.month && year == rhs.year;
+  }
+  bool operator!=(const Time &rhs) const {
+    return day != rhs.day || month != rhs.month || year != rhs.year;
+  }
+  bool operator<(const Time &rhs) const {
+    if (year < rhs.year)
+      return true;
+    else if (year == rhs.year) {
+      if (month < rhs.month)
+        return true;
+      else if (month == rhs.month)
+        if (day < rhs.day)
+          return true;
+    }
+    return false;
+  }
+  bool operator<=(const Time &rhs) const {
+    return operator<(rhs) && operator==(rhs);
+  }
+  bool operator>=(const Time &rhs) const {
+    return !operator<(rhs);
+  }
+  bool operator>(const Time &rhs) const {
+    return !operator<(rhs) && operator!=(rhs);
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const Time& t) {
+    if (t == Time()) {
+      out << "  nan  ";
+    }
+    else {
+      out << std::setfill('0');
+      out << std::setw(2) << int(t.day) << '.'
+         << std::setw(2) << int(t.month) << '.' << t.year;
+      out << std::setfill(' ');
+    }
+    return out;
+  }
+};
+
+//Task 26
 class FileObject {
-private:
-  uint64_t size; //in bytes
-  std::vector<FileObject> dir;
-  std::string name;
-  FileObject *parent;
-
+public:
   enum class Type { File, Directory };
+private:
   Type type;
+  uint64_t size = 0; //in bytes
+  Time modified, minModified;
+  std::string name;
+  std::vector<FileObject> dir;
 
 public:
-  //Task 8
+  Time getMaxTime() const{
+    return modified;
+  }
+  Time getMinTime() const{
+    return minModified;
+  }
+  Type getType() const {
+    return type;
+  }
+  uint64_t getSize() const {
+    return size;
+  }
+  const string &getName() const {
+    return name;
+  }
+  const vector<FileObject> &getDir() const {
+    return dir;
+  }
+
+  template<typename Predicate>
+  Time removeIf(Predicate predicate) {
+    static int depth = -1;
+    depth++;
+    for (auto &a : dir)
+      a.removeIf(predicate);
+    dir.erase(std::remove_if(dir.begin(), dir.end(), predicate), dir.end());
+    if (depth-- == 0) update();
+  }
+
+  template<typename Predicate>
+  FileObject getIf(Predicate predicate) {
+    auto cleared = *this;
+    cleared.removeIf([predicate](const FileObject &fo){return !predicate(fo);});
+    return cleared;
+  }
+
+  void update() {
+    if (type == Type::Directory) {
+      for (auto &a : dir)
+        a.update();
+      size = 0;
+      for (auto &a : dir) {
+        size += a.size;
+      }
+
+      if (!dir.empty()) {
+        modified = std::max_element(dir.begin(), dir.end(),
+        [](const FileObject &a, FileObject &b)
+        { return a.modified < b.modified; })->modified;
+
+        minModified = std::min_element(dir.begin(), dir.end(),
+        [](const FileObject &a, FileObject &b)
+        { return a.minModified < b.minModified
+        && a.minModified != Time(); })->minModified;
+      }
+    }
+  }
+
   void print() {
     // i've got some encoding problem with
     // '│' sign when i using char instead  string
     static std::list<std::string> tree;
 
     if (tree.empty()) {
-      update();
-      std::cout << "Root: ";
+      std::cout << "       Name    Size      Modified"
+                   "\nRoot: ";
     }
     else std::cout << " ";
 
     std::cout << std::setw(0) << name
-            << std::setw(6) << size << " byte"
+            << std::setw(7) << size << " byte  "
+            << modified
               << std::endl;
 
     if (!dir.empty()) {
@@ -56,24 +163,21 @@ public:
     }
   }
 
-  void update() {
-    if (type == Type::Directory) {
-      for (auto &a : dir)
-        a.update();
-      size = 0;
-      for (auto &a : dir)
-        size += a.size;
-    }
-  }
-
   void genRandom(size_t sz) {
+    static int16_t depth = -1;
+    depth++;
+
     std::random_device rd;
     std::default_random_engine re(rd());
     std::uniform_int_distribution<int>
             randsize(sz/4, sz/2), randsizeb(1024, 32768),
-            randtype(0, 3), randname(0, 16), randf(0, 8);
+            randtype(0, 1), randname(0, 16), randf(0, 7),
+            randyear(2013, 2020), randmonth(1, 12), randday(1, 30);
 
-    type = Type(randtype(re) ?  Type::Directory : Type::File);
+    if (depth < 2)
+      type = Type::Directory;
+    else
+      type = Type(randtype(re));
 
     name = std::vector<std::string>(
             {"Lorem", "ipsum", "dolor", "sit", "amet",
@@ -86,6 +190,11 @@ public:
       name.append(std::vector<std::string>(
               {".jpeg", ".dat", ".rar", ".zip",
                ".bin", ".mp3", ".mkv", ".avi"}) [randf(re)]);
+
+      modified.year = randyear(re);
+      modified.month = randmonth(re);
+      modified.day = randday(re);
+      minModified = modified;
     }
     else {
       auto rs = randsize(re);
@@ -96,5 +205,7 @@ public:
         dir.back().genRandom(sz - rs);
       }
     }
+    depth--;
+    if (depth == -1) update();
   }
 };
--- semester2/Lab4/main.cpp
+++ semester2/Lab4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+using namespace std;
 
 #include "Tree.hpp"
 #include "BinaryTree.hpp"
@@ -7,22 +8,36 @@
 
 int main()
 {
+  cout << "Generating random tree" << endl;
   Tree<int> tree;
-
   tree.genRandom(5);
+  cout << endl;
 
+  cout << "Printing all pathes in tree (Task 11):" << endl;
   tree.printAllPaths();
-  tree.print();
+  cout << endl;
 
+  cout << "Printing tree (Task 8?):" << endl;
+  tree.print();
+  cout << endl;
+
+  cout << "Adding node 228 by path {0} and 322 by {0, 0} (Task 3):" << endl;
   tree.addNode(std::list<int>({0}), 228);
   tree.addNode(std::list<int>({0, 0}), 322);
 
   tree.print();
+  cout << endl;
 
+  cout << "Printing subtree gotten by path {0, 0} (Task 2):" << endl;
+  tree.getNode(std::list<int>({0, 0})).print();
+  cout << endl;
+
+  cout << "Removing all values є {228, 322} (Task 15):" << endl;
   tree.removeValue(228);
   tree.removeValue(322);
 
   tree.print();
+  cout << endl;
 
 
   std::random_device rd;
@@ -34,16 +49,43 @@ int main()
     bt.insertValue(randval(re));
   }
 
- // bt.print();
-
-  bt.sortedPrint();
-  bt.preorderPrint();
-
+  cout << "Creating binary tree, and adding of "
+          "10 automatically sorted random values (Task 18):" << endl;
   bt.print();
+  cout << endl;
+
+  cout << "Inorder traversal:" << endl;
+  bt.sortedPrint();
+  cout << endl;
+
+  cout << "Preorder traversal presentation (task 21):" << endl;
+  bt.preorderPrint();
+  cout << endl;
+
+  cout << "Generating random file system tree (Task 26):\n" << endl;
 
   FileObject fs;
   fs.genRandom(10);
   fs.print();
+  cout << endl;
 
+  cout << "Max time: " << fs.getMaxTime() << endl;
+  cout << "Min time: " << fs.getMinTime() << endl;
 
+  cout << "\nFilter: no empty folders\n" << endl;
+
+  fs.getIf([](const FileObject &fo) {
+    return !(fo.getType() == FileObject
+    ::Type::Directory && fo.getDir().empty());
+  }).print();
+
+  cout << "\nFilter: Modified before 22.05.2017, no empty folders\n" << endl;
+
+  fs.getIf([](const FileObject &fo) {
+    return !(fo.getType() == FileObject
+    ::Type::Directory && fo.getDir().empty()) &&
+    ((fo.getType() == FileObject ::Type::File &&
+    fo.getMaxTime() < Time(22,05,2017)) ||
+    fo.getType() == FileObject::Type::Directory);
+  }).print();
 }
