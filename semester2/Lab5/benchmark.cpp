3f99af6d7c7e5c5abbadebd18a644231be0bec95
keyclicker <kolchikcv@ukr.net>
2020-06-15 14:28:32 +0300

    Lab5: small bench fix

--- semester2/Lab5/benchmark.cpp
+++ semester2/Lab5/benchmark.cpp
@@ -12,7 +12,7 @@ template<typename T>
 auto rand(T a, T b) {
   random_device rd;
   default_random_engine re(rd());
-  uniform_int_distribution<T> drand(-a, a);
+  uniform_int_distribution<T> drand(a, b);
   return drand(re);
 }
 
@@ -24,17 +24,17 @@ void name(benchmark::State& state) {\
       gr<int> g(state.range());\
       auto ecnt = (state.range() * state.range()) / 2;\
       for (int i = 0; i < ecnt; ++i)\
-        g.addEdge(rand(long(0), state.range()), \
-                rand(long(0), state.range()), rand(1, 8));\
+        g.addEdge(rand(long(0), state.range()-1), \
+                rand(long(0), state.range()-1), rand(1, 8));\
     state.ResumeTiming();\
     algo(g);\
   };\
 }\
 BENCHMARK(name)\
-->Unit(benchmark::TimeUnit::kMicrosecond)\
+->Unit(benchmark::TimeUnit::kMillisecond)\
 ->RangeMultiplier(2)\
 ->Range(128, 1024) \
-->Iterations()
+->Iterations(1)
 
 GRAPHBENCH(SetIsTree, Graph, Algorithms::isTree);
 GRAPHBENCH(MatrixIsTree, MatrixGraph, Algorithms::isTree);
