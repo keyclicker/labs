ecd10c884eddaabb6eafdabaf9f70d30f0666d8a
keyclicker <kolchikcv@ukr.net>
2021-11-18 02:45:54 +0200

    ruby/lab1 output format changed

--- Term5/Ruby/Lab1/main.rb
+++ Term5/Ruby/Lab1/main.rb
@@ -4,8 +4,7 @@ def fun(a, z, x, p)
     1.3 * 10 ** (-1.6) * Math.sqrt(z + x) + Math::E ** Math::PI
 end
 
-puts "f(a, z, x, p) = ln|z| * sin(p) ^ 0.5 / (a + sqrt(tg(x) ^ 2)) + 1.3 * 10 ^ (-1.6) * Math.sqrt(z + x) + e^pi"
 puts "Input a, z, x, p"
 a, z, x, p = gets.chomp.split.map { |i| i.to_i }
 
-puts fun(a, z, x, p)
+puts "l = #{fun(a, z, x, p)}"
